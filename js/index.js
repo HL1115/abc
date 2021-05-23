e663570dfdf4671679066b238556f2013ec93df9
刘冠军 <1127570788@qq.com>
2021-05-23 14:27:45 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,64 @@
+(function(m, doc) {
+				m.init();
+				// 左划菜单切换
+				m('.mui-off-canvas-wrap').on('tap', '.menu', function(event) {
+					m('.mui-off-canvas-wrap').offCanvas().toggle();
+				});
+				// 退出事件
+				m('.logout')[0].addEventListener('tap', function() {
+					m.openWindow({
+						url: 'login.html',
+					});
+				})
+				
+				m('.mui-table-view-cell').each(function(idx) {
+					this.index = idx;
+				})
+
+				function togg(idx) {
+					m('.con').each(function(i) {
+						if(idx == i){
+							this.style.display = 'block';
+						}else{
+							this.style.display = 'none'
+						}
+					})
+				}
+				togg(0);
+				// m('.mui-inner-wrap').on('tap','button',function(){
+				// 	m.alert('请选择要处理的订单');
+				// })
+				m('.nav').on('tap', '.mui-table-view-cell', function() {
+					togg(this.index)
+					m('.title')[0].innerHTML = this.innerText;
+				})
+				m('.mui-content').on('tap','.addsh',function(){
+					this.nextElementSibling.style.display = 'flex'
+				})
+				m('.modal').on('tap','.close',function(){
+					this.parentNode.parentNode.parentNode.style.display = 'none'
+				})
+				m('.mui-content').on('tap','.modal',function(){
+					this.style.display = 'none'
+				})
+				m('.modal').on('tap','.content',function(e){
+					e.stopPropagation()
+				})
+				// 收款账号
+				$('.acc .edit').click(function(){
+					$(this).parents('.con').find('.modal').css('display','flex')
+				})
+				$('.acc .del').click(function(){
+					m.confirm('确定要删除账号吗')
+				})
+				// 管理员
+				$('.gl .edit').click(function(){
+					$(this).parents('.con').find('.edit-modal').css('display','flex')
+				})
+				$('.qx').click(function(){
+					$(this).parents('.con').find('.fpqx').css('display','flex')
+				})
+				$('.gl .del').click(function(){
+					m.confirm('确定要删除此记录吗')
+				})
+			}(mui, document));
